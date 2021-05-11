5c089b31788b20db9824e1025010dd72da86b582
Doaa-Ismaell <Doaaismael@outlook.com>
2021-05-11 04:58:59 +0200

    add multiple routes

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,22 @@ import Link from "next/link";
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
+    const number1 = (Math.random()*100).toFixed(0);
+    const number2 = (Math.random()*100).toFixed(0);
+    const number3 = (Math.random()*100).toFixed(0);
+    const number4 = (Math.random()*100).toFixed(0);
+
+    return (
     <div>
       <h2>Hey</h2>
-      <Link href={'/numbers/10'}>GO TO NUMBER 10</Link>
+      <Link href={`/numbers/${number1}`}>
+          <p>
+              GO TO NUMBER {number1.toString()}
+          </p>
+           </Link>
+        <Link href={`/numbers/${number2}`}><p>GO TO NUMBER {number2}</p></Link>
+        <Link href={`/numbers/${number3}`}><p>GO TO NUMBER {number3}</p></Link>
+        <Link href={`/numbers/${number4}`}><p>GO TO NUMBER {number4}</p></Link>
     </div>
   )
 }
