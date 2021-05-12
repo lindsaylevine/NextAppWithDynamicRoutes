ac2158ebf3e9eba403dd742565818008e757964a
Doaa-Ismaell <Doaaismael@outlook.com>
2021-05-12 05:09:13 +0200

    use router.push instead of links

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
 import Head from 'next/head'
 import Link from "next/link";
 import styles from '../styles/Home.module.css'
+import {useRouter} from "next/router";
 
 export default function Home() {
+    const router = useRouter();
+
     const number1 = (Math.random()*100).toFixed(0);
     const number2 = (Math.random()*100).toFixed(0);
     const number3 = (Math.random()*100).toFixed(0);
     const number4 = (Math.random()*100).toFixed(0);
 
+    const goToNumber = number => {
+        router.push(`/numbers/${number.toString()}`)
+    }
     return (
     <div>
       <h2>Hey</h2>
-      <Link href={`/numbers/${number1}`}>
-          <p>
+          <p onClick={() => goToNumber(number1)}>
               GO TO NUMBER {number1.toString()}
           </p>
-           </Link>
-        <Link href={`/numbers/${number2}`}><p>GO TO NUMBER {number2}</p></Link>
-        <Link href={`/numbers/${number3}`}><p>GO TO NUMBER {number3}</p></Link>
-        <Link href={`/numbers/${number4}`}><p>GO TO NUMBER {number4}</p></Link>
+        <p onClick={() => goToNumber(number2)}>
+            GO TO NUMBER {number2}</p>
+        <p onClick={() => goToNumber(number3)}>GO TO NUMBER {number3}</p>
+        <p onClick={() => goToNumber(number4)}>GO TO NUMBER {number4}</p>
     </div>
   )
 }
