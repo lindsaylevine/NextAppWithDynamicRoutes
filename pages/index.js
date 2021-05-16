9d7eeaf8b1925a69cd17f757ae74b37f39279600
Doaa-Ismaell <Doaaismael@outlook.com>
2021-05-16 02:53:51 +0200

    simulate branches page behaviour

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,7 @@ import {useRouter} from "next/router";
 export default function Home() {
     const router = useRouter();
 
-    const number1 = (Math.random()*100).toFixed(0);
-    const number2 = (Math.random()*100).toFixed(0);
-    const number3 = (Math.random()*100).toFixed(0);
-    const number4 = (Math.random()*100).toFixed(0);
+    const numbers = Array(3).fill(1).map(() => (Math.random()*100).toFixed(0));
 
     const goToNumber = number => {
         router.push(`/numbers/${number.toString()}`)
@@ -17,13 +14,16 @@ export default function Home() {
     return (
     <div>
       <h2>Hey</h2>
-          <p onClick={() => goToNumber(number1)}>
-              GO TO NUMBER {number1.toString()}
-          </p>
-        <p onClick={() => goToNumber(number2)}>
-            GO TO NUMBER {number2}</p>
-        <p onClick={() => goToNumber(number3)}>GO TO NUMBER {number3}</p>
-        <p onClick={() => goToNumber(number4)}>GO TO NUMBER {number4}</p>
+
+        {
+            numbers.map( num => (
+                <div onClick={() => { console.log("GO TO NUMBER EXECUTED: "); goToNumber(num); }}>
+                    <Link href={`/numbers/${num}`} locale={router.locale}>
+                        <p> Go To {num}</p>
+                    </Link>
+                </div>
+            ))
+        }
     </div>
   )
 }
